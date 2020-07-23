b7bb8c4cecac1ab949b9c5bdefe5e5a66a557101
varunks99 <varunshiri99@gmail.com>
2020-07-23 13:09:23 +0530

    test center ui

--- ehr-api/routes/organisation/testcenter.js
+++ ehr-api/routes/organisation/testcenter.js
@@ -13,75 +13,73 @@ router.use(upload());
 
 //All routes have prefix '/organisation/testcenter'
 router.get('/login', (req, res) => {
-    res.render('org/org-login', {
-        org: 'testcenter'
-    });
+  res.render('org/org-login', {
+    org: 'testcenter'
+  });
 });
 
 router.post('/login', passport.authenticate('local', {
-    successRedirect: '/organisation/testcenter',
-    failureRedirect: '/organisation/testcenter/login'
+  successRedirect: '/organisation/testcenter',
+  failureRedirect: '/organisation/testcenter/login'
 }), (req, res) => {
 
 });
 
 router.use((req, res, next) => {
-    if (req.user.type == 'testcenter')
-        next();
-    else
-        res.redirect('/');
+  if (req.user.type == 'testcenter')
+    next();
+  else
+    res.redirect('/');
 });
 
 router.get('/', (req, res) => {
-    res.render('org/testcenter', {
-        response: {}
-    });
-    
-    res.sendFile(__dirname + "org/testcenter"); //text extraction
-    
+  res.render('org/testcenter', {
+    response: {}
+  });
+
+  res.sendFile(__dirname + "org/testcenter"); //text extraction
+
 });
 
 router.post('/addreport', (req, res) => {
-    var MedicalID = req.body.medicalID;
-    var bloodgroup = req.body.bloodGroup;
-    var bloodpressure = req.body.bloodPressure;
-    var haemoglobin = req.body.haemoglobin;
-    var sugarlevel = req.body.sugarlevel;
-    var links = req.body.links || ' ';
-    var report = 'Blood Group:' + bloodgroup + ' ' + 'Blood Pressure:' + bloodpressure + ' ' + 'Haemoglobin:' + haemoglobin + ' ' + 'Glucose:' + sugarlevel;
-    console.log(report);
-    var doc = {
-        'medicalID': MedicalID,
-        'report': report,
-        'links': links
-    }
+  var MedicalID = req.body.medicalID;
+  var bloodgroup = req.body.bloodGroup;
+  var bloodpressure = req.body.bloodPressure;
+  var haemoglobin = req.body.haemoglobin;
+  var sugarlevel = req.body.sugarlevel;
+  var links = req.body.links || ' ';
+  var report = 'Blood Group:' + bloodgroup + ' ' + 'Blood Pressure:' + bloodpressure + ' ' + 'Haemoglobin:' + haemoglobin + ' ' + 'Glucose:' + sugarlevel;
+  console.log(report);
+  var doc = {
+    'medicalID': MedicalID,
+    'report': report,
+    'links': links
+  }
 
-    //------------textextraction
-    if (req.files) {
-      var file = req.files.file; 
-      var fileName = file.name;
-      
-      file.mv("uploads/" + fileName, function (err) { // moving file to uploads folder
-        if (err) {// if error occurs run this
-          console.log("File was not uploaded!!");
-          res.send(err);
-        } 
-        
-        else {
-          console.log("file uploaded");
-          const pdffile = fs.readFileSync("uploads/" + fileName); //read the file
+  //------------textextraction
+  if (req.files) {
+    var file = req.files.file;
+    var fileName = file.name;
 
-          pdfparse(pdffile).then(function (data) {  //text-extraction function
-            var rawtext = data.text;    //all the extracted text is stored in "rawtext" variable
-            console.log(rawtext);       //extracted text can be seen in the console
-          });
-        }
-      }); 
-    }   
-    //------------textextraction
+    file.mv("uploads/" + fileName, function (err) { // moving file to uploads folder
+      if (err) { // if error occurs run this
+        console.log("File was not uploaded!!");
+        res.send(err);
+      } else {
+        console.log("file uploaded");
+        const pdffile = fs.readFileSync("uploads/" + fileName); //read the file
 
-    
-    ehrTestCenter.addrLReport(req, res, doc);
+        pdfparse(pdffile).then(function (data) { //text-extraction function
+          var rawtext = data.text; //all the extracted text is stored in "rawtext" variable
+          console.log(rawtext); //extracted text can be seen in the console
+        });
+      }
+    });
+  }
+  //------------textextraction
+
+
+  // ehrTestCenter.addrLReport(req, res, doc);
 });
 
 module.exports = router;
