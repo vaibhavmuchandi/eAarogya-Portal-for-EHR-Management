b11e427376da6497a8fe336b6d17274b69748d5c
Vaibhav Muchandi <vaibhavmm@hotmail.com>
2020-08-03 10:26:02 +0530

    added_ipfs_api_files

---
+++ Aarogya_IPFS_API/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    libp2pkey : "bbaareyepqigb7kx5uwt6swjzuas7adjuc4gvnlpcdrcrl3autdwdu3nm6kujdbj2kwlkbeyhrzjiow5pktrgq6wxdhvqetlfcdm7xxcuizhmnkerqu5flfvasmdy4uuhloxvjytipllrt2ycjvsrbwp33rkemtwg",
+    bucketKey : "b3nm555qn6ysqeoiymsiuznsvni",
+    path: "bafkreicxwrkg37vacfv55pvzgwypajqtrtl74vfdnt3unwnmja3m5g3cxy",
+    firebaseAPI: "AIzaSyCZMvVLt-iEDrWpjoMLzOlt-XEuEGgmP0I",
+    firebaseauthDomain: "earogya-ipfs.firebaseapp.com",
+    firebaseDbUrl: "https://earogya-ipfs.firebaseio.com",
+    firebaseProjectId: "earogya-ipfs",
+    firebaseStorageBucket: "earogya-ipfs.appspot.com",
+    firebasemsgId: "793982089641",
+    firebaseAppId: "1:793982089641:web:03db569f5609c57989acb8"
+}
